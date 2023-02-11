15cf6f8d8f57204051f0d3c5ec72166ba3680fed
zzlev <gpalektzja@gmail.com>
2023-02-11 11:29:55 +0900

    Add relationship methods.

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -29,4 +29,19 @@ public class Order {
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItems(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
