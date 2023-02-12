11478bfe87b519aca3b7370a669c205732bc8ff8
zzlev <gpalektzja@gmail.com>
2023-02-12 16:02:15 +0900

    Add createOrderItem.

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,6 +26,16 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
     public void cancel() {
         item.addStock(count);
     }
