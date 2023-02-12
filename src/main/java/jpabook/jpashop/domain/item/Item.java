6eec8f3f8a54a0d222069ec0d080d0d80a2491e9
zzlev <gpalektzja@gmail.com>
2023-02-12 14:51:20 +0900

    Add stock domain methods.

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int resStock = this.stockQuantity - quantity;
+        if (resStock < 0) {
+            throw new NotEnoughStockException("Need more stocks.");
+        }
+        this.stockQuantity = resStock;
+    }
 }
