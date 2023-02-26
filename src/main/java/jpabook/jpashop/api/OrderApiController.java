ca6dba7cd27eac19c05fa8fb91678f2239fdfa72
zzlev <gpalektzja@gmail.com>
2023-02-26 17:20:43 +0900

    Add v1 orders.

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+        }
+        return orders;
+    }
+}
